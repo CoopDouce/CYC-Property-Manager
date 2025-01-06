d949fa48c809c486a25d00b6c9bf23f300c6a61e
Christian Guzman <cguzman199@gmail.com>
2025-01-05 20:11:33 -0600

    added api key

---
+++ server/api/mapsAPI.js
@@ -0,0 +1,30 @@
+var _awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+
+import { router } from 'express';
+import fetch from 'node-fetch';
+const router = Router();
+
+router.get('/maps', (req, res) => _awaiter(void 0, void 0, void 0, function* () {
+    const { address } = req.query;
+    const myHeaders = new fetch.Headers();
+    myHeaders.append("apikey", 'AIzaSyAOeDzHH74zqFqfqgKOdnx8LFa1AhtuoVM');
+
+    const requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.GOOGLE_MAPS_API_KEY}`, requestOptions)
+        .then(response => response.json())
+        .then(result => res.json(result))
+        .catch(error => res.json(error));
+}
+));
---
+++ server/api/mapsAPI.ts
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+import { Request, Response } from 'express-serve-static-core';
+import fetch, { Headers } from 'node-fetch';
+
+const router = Router();
+
+router.post('/getDirections', async (req: Request, res: Response) => {
+    const {origin, destination} = req.body;
+
+    const myHeaders = new Headers();
+    myHeaders.append("apikey", "AIzaSyAOeDzHH74zqFqfqgKOdnx8LFa1AhtuoVM");
+
+    const requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow' as RequestRedirect
+    };
+
+    try {
+        const response = await fetch("https://maps.googleapis.com/maps/api/directions/json", requestOptions);
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: 'Server Error'});
+    }
+}
+);
+
+export default router;
